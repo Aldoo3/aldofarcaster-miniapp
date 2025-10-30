495dcb992b9df577e4f40f50dcfce8e41dca4b8a
Aldoo3 <aldoremifa42@gmail.com>
2025-10-30 21:15:17 +0100

    Update route.ts

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,29 +1,27 @@
-import { NextResponse } from "next/server";
-import { APP_URL } from "../../../lib/constants";
+const appUrl = process.env.NEXT_PUBLIC_SITE_URL;
+
+const farcasterConfig = {
+  accountAssociation: {
+    header: "eyJmaWQiOjkxNTYyNywidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDc2ZkNhRmI4OTY4YmJmYTM0NjRhYzM1RTdDMTY2NjdhNGQ1N2MxZDkifQ",
+    payload: "eyJkb21haW4iOiJhbGRvZmFyY2FzdGVyLW1pbmlhcHAtdnplby52ZXJjZWwuYXBwIn0=",
+    signature: "Jiz8lPjMFye5TlKkT85Icy7jQo9+hvrn7J2aWAkB/Zt7ej1ARXr7s0ioug6xwy0yrYz6cYP/EXs5dPNcRol8ZBw="
+  },
+  frame: {
+    version: "1",
+    name: "Aldo Farcaster Mini App",
+    iconUrl: `${appUrl}/images/icon.png`,
+    homeUrl: `${appUrl}`,
+    imageUrl: `${appUrl}/images/feed.png`,
+    screenshotUrls: [],
+    tags: ["farcaster", "miniapp", "template"],
+    primaryCategory: "developer-tools",
+    buttonTitle: "Launch App",
+    splashImageUrl: `${appUrl}/images/splash.png`,
+    splashBackgroundColor: "#ffffff",
+    webhookUrl: `${appUrl}/api/webhook`
+  }
+};
 
 export async function GET() {
-  const farcasterConfig = {
-  // TODO: Add your own account association
-
-    {
-  "accountAssociation": {
-    "header": "eyJmaWQiOjkxNTYyNywidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDc2ZkNhRmI4OTY4YmJmYTM0NjRhYzM1RTdDMTY2NjdhNGQ1N2MxZDkifQ",
-    "payload": "eyJkb21haW4iOiJhbGRvZmFyY2FzdGVyLW1pbmlhcHAtdnplby52ZXJjZWwuYXBwIn0",
-    "signature": "Jiz8lPjMFye5TlKkT85Icy7jQo9+hvrn7J2aWAkB/Zt7ej1ARXr7s0ioug6xwy0yrYz6cYP/EXs5dPNcRol8ZBw="
-  },
-  "frame": {
-    "version": "1",
-    "name": "Example Frame",
-    "iconUrl": "https://aldofarcaster-miniapp-vzeo.vercel.app/icon.png",
-    "homeUrl": "https://aldofarcaster-miniapp-vzeo.vercel.app",
-    "imageUrl": "https://aldofarcaster-miniapp-vzeo.vercel.app/image.png",
-    "buttonTitle": "Check this out",
-    "splashImageUrl": "https://aldofarcaster-miniapp-vzeo.vercel.app/splash.png",
-    "splashBackgroundColor": "#eeccff",
-    "webhookUrl": "https://aldofarcaster-miniapp-vzeo.vercel.app/api/webhook"
-  }
-}
-
-
-  return NextResponse.json(farcasterConfig);
+  return Response.json(farcasterConfig);
 }
