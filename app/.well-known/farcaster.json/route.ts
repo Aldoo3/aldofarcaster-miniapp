5256bbf4f2697d7ec0f482cf8368bd2740149d8c
Aldoo3 <aldoremifa42@gmail.com>
2025-10-30 21:09:24 +0100

    Update route.ts

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -4,26 +4,26 @@ import { APP_URL } from "../../../lib/constants";
 export async function GET() {
   const farcasterConfig = {
   // TODO: Add your own account association
-    accountAssociation: {
-    "header": "eyJmaWQiOjg4NzE3OSwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweGJBMkE1MkFBYkY2N2JhMzljRDlCYkU3ODVCZTk1Qzc5NGRmNWVGQWIifQ",
-    "payload": "eyJkb21haW4iOiJmYXJjYXN0ZXItbWluaWFwcC10ZW1wbGF0ZS1zYWdlLnZlcmNlbC5hcHAifQ",
-    "signature": "bZjJKF4S1YesIPkGdwsLNWtQu1+MKVJD9suMpWxIFphtAmrWXm687SlonKdagl9WOdM5INB3EYtD6VYvJtqHrhw="
+
+    {
+  "accountAssociation": {
+    "header": "eyJmaWQiOjkxNTYyNywidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDc2ZkNhRmI4OTY4YmJmYTM0NjRhYzM1RTdDMTY2NjdhNGQ1N2MxZDkifQ",
+    "payload": "eyJkb21haW4iOiJhbGRvZmFyY2FzdGVyLW1pbmlhcHAtdnplby52ZXJjZWwuYXBwIn0",
+    "signature": "Jiz8lPjMFye5TlKkT85Icy7jQo9+hvrn7J2aWAkB/Zt7ej1ARXr7s0ioug6xwy0yrYz6cYP/EXs5dPNcRol8ZBw="
   },
-    frame: {
-      version: "1",
-      name: "Farcaster MiniApp",
-      iconUrl: `${APP_URL}/images/icon.png`,
-      homeUrl: `${APP_URL}`,
-      imageUrl: `${APP_URL}/images/feed.png`,
-      screenshotUrls: [],
-      tags: ["Base", "farcaster", "miniapp", "template"],
-      primaryCategory: "developer-tools",
-      buttonTitle: "Launch Template",
-      splashImageUrl: `${APP_URL}/images/splash.png`,
-      splashBackgroundColor: "#ffffff",
-      webhookUrl: `${APP_URL}/api/webhook`,
-    },
-  };
+  "frame": {
+    "version": "1",
+    "name": "Example Frame",
+    "iconUrl": "https://aldofarcaster-miniapp-vzeo.vercel.app/icon.png",
+    "homeUrl": "https://aldofarcaster-miniapp-vzeo.vercel.app",
+    "imageUrl": "https://aldofarcaster-miniapp-vzeo.vercel.app/image.png",
+    "buttonTitle": "Check this out",
+    "splashImageUrl": "https://aldofarcaster-miniapp-vzeo.vercel.app/splash.png",
+    "splashBackgroundColor": "#eeccff",
+    "webhookUrl": "https://aldofarcaster-miniapp-vzeo.vercel.app/api/webhook"
+  }
+}
+
 
   return NextResponse.json(farcasterConfig);
 }
